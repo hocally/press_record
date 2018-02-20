b050596efc728c3bcc6708b94f24d74ce289c833
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2018-02-19 18:10:14 -0600

    Realized we can't use the nano, lel
    Add a bunch of TODOs, a pinout diagram, and some other minor fixes in main()

--- remote_button_code/remote_button_code.ino
+++ remote_button_code/remote_button_code.ino
@@ -1,16 +1,31 @@
-#include <MIDI.h>
+/*TODO
+ * Change digitalRead()s for interrupts
+ * Add LED function
+ * Make LED function snazzy
+ * Add RF comms library
+ * Add RF functionality
+ * Actually test it lol
+*/
+#include <Keyboard.h>
+
+#define ON_BUTTON 4 //Pin connected to the "on" button. Vector is PD4
+#define OFF_BUTTON 5 //Pin connected to the "off" button. Vector is PC6
+#define LED 6 //Pin connected to LED, should probably be wired in series with a 330ish Ohm resistor. Vector is PD7
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(ON_BUTTON, INPUT_PULLUP);
+  pinMode(OFF_BUTTON, INPUT_PULLUP);
+  pinMode(LED, OUTPUT); digitalWrite(LED, LOW);
+  Keyboard.begin();
 }
 
 void loop() {
-  //TODO assign buttons for the digital reads
   // put your main code here, to run repeatedly:
-	if(digitalRead() == LOW){
-		Keyboard.write(r) //sends the keyboard shortcut for record
+	if(digitalRead(ON_BUTTON) == LOW){
+		Keyboard.write('r'); //sends the keyboard shortcut for record
 	}
-	if(digitalRead() == LOW{
-		keyboard.write(' ') //sends the keyboard shortcut for stop
+	if(digitalRead(OFF_BUTTON) == LOW){
+		Keyboard.write(' '); //sends the keyboard shortcut for stop
 	}
 }
