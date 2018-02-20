862f837a41ad6180a44796c1d475f5d6a8b3284f
hocally <henry.ocallaghan@gmail.com>
2018-02-20 17:52:49 -0600

    Minor tweaks and adjustments

--- remote_button_code/remote_button_code.ino
+++ remote_button_code/remote_button_code.ino
@@ -19,49 +19,56 @@ enum connection_state { //Not sure if this will be useful, but could make it way
     PACKET_RECIEVED
 };
 
+NRFLite radio;
+uint8_t data;
+
 bool stateChange;
 
 connection_state state = NO_CONNECTION;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(ON_BUTTON, INPUT_PULLUP); attachInterrupt(digitalPinToInterrupt(ON_BUTTON), record, LOW);
-  pinMode(OFF_BUTTON, INPUT_PULLUP); attachInterrupt(digitalPinToInterrupt(OFF_BUTTON), pause, LOW);
-  pinMode(LED, OUTPUT); digitalWrite(LED, LOW);
-  Keyboard.begin();
-  state = NO_CONNECTION;
-  stateChange = true;
+    // put your setup code here, to run once:
+    pinMode(ON_BUTTON, INPUT_PULLUP); attachInterrupt(digitalPinToInterrupt(ON_BUTTON), record, LOW);
+    pinMode(OFF_BUTTON, INPUT_PULLUP); attachInterrupt(digitalPinToInterrupt(OFF_BUTTON), pause, LOW);  
+    pinMode(LED, OUTPUT); digitalWrite(LED, LOW);
+    Keyboard.begin();
+    Serial.begin(115200);
+    radio.init(1, 9, 10);
+    state = NO_CONNECTION;
+    stateChange = true;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(stateChagnge) {
+    // put your main code here, to run repeatedly:
+    while(radio.hasData()) {
+        //Function should do comms stuff here
+    }
+    if(stateChagnge) {
     stateChange = false;
     ledState(state);
-  }
+    }
 }
 
 void ledState(int i) {
-  switch (i) {
-    case 0:
-      //Need to do something cool here with hardware timers
-      
-      break;
-    case 1:
-      //Need to do something cool here with hardware timers
-      break;
-    case 2:
-      //Need to do something cool here with hardware timers
-      break;
-    case 3:
-      //Need to do something cool here with hardware timers
-      break;
-    case 4:
-      //Need to do something cool here with hardware timers
-      break;
-    default:
-      //Need to do something cool here with hardware timers
-  }
+    switch (i) {
+        case 0:
+            //Need to do something cool here with hardware timers
+            break;
+        case 1:
+            //Need to do something cool here with hardware timers
+            break;
+        case 2:
+            //Need to do something cool here with hardware timers
+            break;
+        case 3:
+            //Need to do something cool here with hardware timers
+            break;
+        case 4:
+            //Need to do something cool here with hardware timers
+            break;
+        default:
+            //Need to do something cool here with hardware timers
+    }
 }
 
 void record() {
